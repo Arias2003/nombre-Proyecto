fd318e2fc4834088b4e519ed86b4ada9bc8e3482
Sebastián Arias <167481206+Arias2003@users.noreply.github.com>
2025-06-20 14:37:50 -0500

    Código Dart - Carrito de compras

---
+++ CarritoCompra.dart
@@ -0,0 +1,54 @@
+class Producto{
+  String _nombre;
+  double _precio;
+
+  Producto(this._nombre, this._precio);
+
+  String get nombre => this._nombre;
+  set nombre(String nombProducto){
+    if(nombProducto.length >= 3){
+      this._nombre = nombProducto;
+    }else{
+      print('Nuevo nombre de producto es muy corto');
+    }
+  }
+
+  double get precio => this._precio;
+  void descuentoPrecio(double monto){
+    this._precio -= monto;
+  }
+
+  void mostrarInformacion(){
+    print('Nombre del Producto: $_nombre, Precio: ${_precio.toStringAsFixed(2)}');
+  }
+}
+
+class ProductoDescuento extends Producto{
+  double _porcentajeDescuento;
+
+  ProductoDescuento(String nombre, double precio, this._porcentajeDescuento) : super(nombre, precio);
+
+  
+  void aplicarDescuento(){
+    if(this._porcentajeDescuento > 0){
+      double descuento = precio * this._porcentajeDescuento;
+      descuentoPrecio(descuento);
+      print('Se aplicó un descuento del ${(this._porcentajeDescuento * 100).toStringAsFixed(0)}%: \$${descuento.toStringAsFixed(2)}');
+print('Total: \$${precio.toStringAsFixed(2)}');      
+    }else{
+      print('El descuento no se pude aplicar');
+    }
+  }
+
+  @override
+  void mostrarInformacion(){
+    print('Nombre del Producto con descuento: $nombre');
+  print('Precio final: \$${precio.toStringAsFixed(2)}');
+  }
+}
+
+void main(){
+  var iphone = ProductoDescuento('APPLE', 2000, 0.05);
+  iphone.mostrarInformacion();
+  iphone.aplicarDescuento();
+}
